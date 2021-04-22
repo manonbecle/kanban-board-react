126268d41e1ef12bfcc25ebff48eacd6e0c13381
manonbecle <manon.becle@gmail.com>
2021-04-22 22:18:27 +0200

    init - Title and favicon

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,14 +2,12 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    Init
   </div>
 );
 
