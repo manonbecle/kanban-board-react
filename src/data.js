54534190eaa0e26e448bdbaa984bc291945ae946
manonbecle <manon.becle@gmail.com>
2021-04-24 11:27:36 +0200

    data file created

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    title: 'Creer un fichier de data pour dynamiser l\'affichage',
+    priority: '2',
+    assigment: 'Julien B',
+  },
+  {
+    id: 2,
+    title: 'Responsive design',
+    priority: '3',
+    assigment: 'Vincent F',
+  },
+  {
+    id: 3,
+    title: 'Installer Redux pour gérer les changements du state',
+    priority: '1',
+    assigment: 'Estelle L',
+  },
+  {
+    id: 4,
+    title: 'Gestion du Drag and drop et donc de la position de chaque \'Card\' dans une liste',
+    priority: '1',
+    assigment: 'Esla M',
+  },
+  {
+    id: 5,
+    title: 'Ajouter les composants et créer le css de ceux-ci',
+    priority: '4',
+    assigment: 'Esla M',
+  },
+];
