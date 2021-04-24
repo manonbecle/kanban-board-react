81d78edbf0fe50251c8b0bf3a9b725e7f7370c59
manonbecle <manon.becle@gmail.com>
2021-04-24 11:33:21 +0200

    redux installation

---
+++ src/reducers/tasksReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  // ici le state initial
+};
+
+function tasksReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default tasksReducer;
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import reducer from 'src/reducers/tasksReducer';
+
+const store = createStore(
+  // reducer
+  reducer,
+  // enhancer
+  devToolsEnhancer(),
+);
+
+export default store;
